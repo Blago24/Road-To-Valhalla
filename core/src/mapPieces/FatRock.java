905ba30f9c3432291db7127c85256aa6831dc5fb
Blago24 <blago102@gmail.com>
2017-04-22 23:00:55 +0300

    pak teo mi go kaza

--- core/src/mapPieces/FatRock.java
+++
@@ -1,9 +0,0 @@
-package mapPieces;
-
-public class FatRock extends BackgroundPiece{
-
-	public FatRock(float xPosition, float yPosition) {
-		super(xPosition, yPosition, false);
-	}
-	
-}
--- core/src/mapPieces/RockyGrass.java
+++
@@ -1,9 +0,0 @@
-package mapPieces;
-
-public class RockyGrass extends BackgroundPiece{
-
-	public RockyGrass(float xPosition, float yPosition) {
-		super(xPosition, yPosition, true);
-	}
-
-}
